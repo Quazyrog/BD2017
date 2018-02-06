808c17dd022a6fcc30c83d32ed0f6c488a84876b
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2018-02-07 00:56:53 +0100

    Added paging to /api/logs/search.php

--- Application/root/api/logs/search.php
+++ Application/root/api/logs/search.php
@@ -6,9 +6,17 @@ require "../../../vendor/autoload.php";
 $foo = new class extends APIEndpoint {
     protected function main(array $args): bool
     {
+        $page = 0;
+        if (isset($args["page"])) {
+            if (!is_numeric($args["page"]))
+            $this->message_("Page must be numeric");
+            $page = intval($args["page"]);
+        }
+
         $compiler = \searching\QueryCompiler::BuildDefaultCompiler($this->database);
         try {
-            $stm_str = $compiler->compile($_REQUEST["query"]) . " LIMIT 50";
+            $stm_str = $compiler->compile($_REQUEST["query"]) . " LIMIT " . \config\FETCH_PAGE_SIZE
+                . " OFFSET " . $page * \config\FETCH_PAGE_SIZE;
         } catch (\searching\SyntaxError $e) {
             $this->message_("Syntax error");
             $this->result_($e);
